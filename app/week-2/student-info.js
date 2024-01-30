c0fad8c1e849a08b1c421cb8c5a00d90de1802c4
Ammar-Khan08 <130613802+Ammar-Khan08@users.noreply.github.com>
2024-01-30 16:18:55 -0700

    completed week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return (
+        <div>
+            <p>Ammar Khan</p>
+            <Link href="https://github.com/Ammar-Khan08/cprg306-assignments">Link to my github repo</Link>
+        </div>
+    )
+}
