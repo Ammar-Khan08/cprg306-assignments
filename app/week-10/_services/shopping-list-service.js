07afbc28cb580f2ebb468b4c6011d9968ad7e143
Ammar-Khan08 <130613802+Ammar-Khan08@users.noreply.github.com>
2024-04-02 12:27:54 -0600

    Week 10 final commit

---
+++ app/week-10/_services/shopping-list-service.js
@@ -0,0 +1,24 @@
+import { db } from "../_utils/firebase";
+import { collection, getDocs, addDoc, query, doc, deleteDoc } from "firebase/firestore";
+
+export async function getItems(userId) {
+  const itemsCollectionRef = collection(db, "users", userId, "items");
+  const q = query(itemsCollectionRef);
+  const querySnapshot = await getDocs(q);
+  const items = [];
+  querySnapshot.forEach((doc) => {
+    items.push({ id: doc.id, ...doc.data() });
+  });
+  return items;
+}
+
+export async function addItem(userId, item) {
+  const itemsCollectionRef = collection(db, "users", userId, "items");
+  const docRef = await addDoc(itemsCollectionRef, item);
+  return docRef.id; // Return the ID of the newly added item
+}
+
+export async function deleteItem(userId, itemId) {
+    const itemDocRef = doc(db, "users", userId, "items", itemId);
+    await deleteDoc(itemDocRef);
+  }
---
+++ app/week-10/shopping-list/item.js
@@ -0,0 +1,26 @@
+function Item({ id, name, quantity, category, onSelect, onDelete }) {
+  return (
+    <div className="bg-slate-900 mb-4 mt-4 ml-2 container p-2 max-w-sm flex justify-between">
+      <div onClick={() => onSelect(name)}>
+        <ul>
+          <li className="text-xl font-bold">{name}</li>
+          <li className="text-sm">
+            Buy {quantity} in {category}
+          </li>
+        </ul>
+      </div>
+
+      <button
+          onClick={(e) => {
+            e.stopPropagation();
+            onDelete(id);
+          }}
+          className="bg-red-600 p-1 h-8 w-8 font-bold self-center active:bg-red-500"
+        >
+          X
+        </button>
+    </div>
+  );
+}
+
+export default Item;
